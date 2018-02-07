43aedfb139d046934d612fdcfac1d302dd28ccf8
Lightnet <darkneter@gmail.com>
2018-02-07 13:34:19 -0800

    Test Voxel Place check and create place location when mouse press.

--- scripts/CamVoxel.gd
+++ scripts/CamVoxel.gd
@@ -1,55 +1,73 @@
 extends Camera
 
+const BlockVoxel = preload("res://objects/KBVoxel.tscn")
+
 var offset = Vector3(0.5,0.5,0.5)
 var ray_length = 10
 var point
 var ROT_SPEED = 0.1
 var bhit = false
 
+var placeposition = Vector3(0,0,0)
+
+var label01
+var label02
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	point = get_node("../MeshInstance")
+	label01 = get_node("../../Control/Label")
+	label02 = get_node("../../Control/Label2")
 	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
 
 func _input(ev):
 	#print(ev)
 	#if ev is InputEventMouseButton:
 	if ev is InputEventMouseMotion:
-		var camera = self #get_node("Camera")
+		#get camera
+		var camera = self #get_node("Camera
+		#get camera view mouse position
 		var from = camera.project_ray_origin(ev.position)
+		#face direction vector3 placeholder position
 		var to = from + camera.project_ray_normal(ev.position) * ray_length
-		#var view = get_viewport().get_camera().unproject_position(get_global_transform().origin)
+		
+		#setup for y axis zero plane
 		var view = camera.project_ray_normal(ev.position)
+		#normal vector to 0.0 - 1.0
 		var dir = view.normalized()
+		#Camera Position and direction target position
 		var distance = - camera.transform.origin.y / dir.y;
+		# caluate y axis convert to x and z axis plane from mouse x and y position
 		var pos = Vector3(camera.transform.origin.x,camera.transform.origin.y,camera.transform.origin.z) + dir * distance
 		
+		#ray cast hit block
 		var directState = PhysicsServer.space_get_direct_state(camera.get_world().get_space())
 		var result = directState.intersect_ray(from, to, [self])
+		
+		bhit = false
 		if result.size() != 0:
+			bhit = true
 			#print("?")
-			print(result)
+			#print(result)
 			#result.normal
-			#result.position
-			if result.normal.x == 1 or result.normal.x == -1:
-				pos.x = round(pos.x) + result.normal.x * 0.5
-			if result.normal.y >= 0.8 or result.normal.y <= -0.8:
-				pos.y = round(pos.y) + result.normal.y * 2
-			if result.normal.z >= 0.8 or result.normal.z <= -0.8:
-				pos.z = round(pos.z) + result.normal.z * 0.5
+			pos = result.position
+			label01.set_text("Normal x:" + String(result.normal.x)+" y:" + String(result.normal.y) +" x:" + String(result.normal.z))
+			label02.set_text("Hit x:" + String(pos.x)+" y:" + String(pos.y) +" x:" + String(pos.z))
 			pos.x = round(pos.x)
 			pos.y = round(pos.y)
 			pos.z = round(pos.z)
+			if result.normal.x != 0:
+				pos.x = round(pos.x) + result.normal.x * 1
+			if result.normal.y != 0:
+				pos.y = round(pos.y) + result.normal.y * 1
+			if result.normal.z != 0:
+				pos.z = round(pos.z) + result.normal.z * 1
+			print(pos)
 		#point.transform.origin = to
-		#if bhit:
-			#pass
-		#else:
+		if bhit:
+			placeposition = pos
+			pass
 		point.transform.origin = pos
 		#print("Move")
 		
@@ -77,5 +95,10 @@ func _input(ev):
 	if ev is InputEventMouseButton:
 		if ev.button_index == BUTTON_LEFT and ev.pressed:
 			print("LEFT MOUSE PRESS")
+			
+			var objvoxel = BlockVoxel.instance()
+			objvoxel.transform.origin = placeposition
+			get_node("/root/Node/Spatial").add_child(objvoxel)
+			
 			pass
 		pass
