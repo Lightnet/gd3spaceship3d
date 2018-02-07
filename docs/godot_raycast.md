fe234aafdb04ea288daccf94f71da8034b80f132
Lightnet <darkneter@gmail.com>
2018-02-07 09:54:35 -0800

    y axis zero plane ray tested.

--- docs/godot_raycast.md
+++ docs/godot_raycast.md
@@ -7,3 +7,31 @@
  * http://www.gamefromscratch.com/post/2015/12/08/Godot-Engine-Tutorial-Part-17-3D-Ray-casting.aspx
  * https://github.com/godotengine/godot/issues/2217
 
+ * https://github.com/godotengine/godot/issues/1725
+
+
+
+# ray cast without plane mesh and y axis zero plane
+```
+var camera = self #get_node("Camera")
+var view = camera.project_ray_normal(ev.position) #convert 2D mouse position to vector3.
+var dir = view.normalized()
+var distance = - camera.transform.origin.y / dir.y; #Change axis to any x,y,z
+var pos = Vector3(camera.transform.origin.x,camera.transform.origin.y,camera.transform.origin.z) + dir * distance #convert to plane zero y axis to zero
+```
+
+# ray cast hit
+```
+var ray_length = 10
+
+var camera = self #get_node("Camera")
+var from = camera.project_ray_origin(ev.position)
+var to = from + camera.project_ray_normal(ev.position) * ray_length
+
+var directState = PhysicsServer.space_get_direct_state(camera.get_world().get_space())
+var result = directState.intersect_ray(from, to, [self])
+
+if result.size() != 0:
+    print("Hit")
+    pass
+```
--- scripts/CamVoxel.gd
+++ scripts/CamVoxel.gd
@@ -2,10 +2,12 @@ extends Camera
 
 var offset = Vector3(0.5,0.5,0.5)
 var ray_length = 10
+var point
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	point = get_node("../MeshInstance")
 	pass
 
 #func _process(delta):
@@ -20,16 +22,19 @@ func _input(ev):
 		var camera = self #get_node("Camera")
 		var from = camera.project_ray_origin(ev.position)
 		var to = from + camera.project_ray_normal(ev.position) * ray_length
-		var point = get_node("../MeshInstance")
+		#var view = get_viewport().get_camera().unproject_position(get_global_transform().origin)
+		var view = camera.project_ray_normal(ev.position)
+		var dir = view.normalized()
+		var distance = - camera.transform.origin.y / dir.y;
+		var pos = Vector3(camera.transform.origin.x,camera.transform.origin.y,camera.transform.origin.z) + dir * distance
 		
 		var directState = PhysicsServer.space_get_direct_state(camera.get_world().get_space())
 		var result = directState.intersect_ray(from, to, [self])
 		if result.size() != 0:
-			print("?")
+			#print("?")
 			pass
-		
-		
-		point.transform.origin = to
+		#point.transform.origin = to
+		point.transform.origin = pos
 		#print("Move")
 	#point.transform.origin = point
 	#print(to)
